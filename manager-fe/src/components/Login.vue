36e41870ad4e8d2bce82fd10a5fd3a9fceb62bea
Frank <841290355@qq.com>
2022-04-19 13:26:45 +0800

    基本路由

---
+++ manager-fe/src/components/Login.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div>Login</div>
+    <el-button @click="goHome">回首页</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  methods: {
+    goHome () {
+      this.$router.push('/welcome')
+    }
+  },
+}
+</script>
+
+<style>
+</style>
---
+++ manager-fe/src/components/Welcome.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div>欢迎来到主界面</div>
+    <!-- <el-button>去登录<el-button> -->
+    <router-link to="/login">去登录</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Welcome'
+}
+</script>
+
+<style>
+</style>
