2e18c5c78cf00697551f4494da0428e644e51b5b
Frank <841290355@qq.com>
2022-04-23 21:34:04 +0800

    storage封装

--- manager-fe/src/App.vue
+++ manager-fe/src/App.vue
@@ -3,7 +3,14 @@
 </template>
 <script>
 export default {
-  name: 'app'
+  name: 'app',
+  mounted () {
+    this.$storage.setItem('userInfo', { age: 30, name: 'jacl' })
+    console.log('user=>', this.$storage.getItem('userInfo'))
+    this.$storage.clearItem('name')
+    this.$storage.clearAll()
+  },
+
 }
 </script>
 
